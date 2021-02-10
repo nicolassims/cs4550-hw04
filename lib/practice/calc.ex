d6c04ea4545e7a3c2c7381a8b1eb4e44af6f6fc9
nicolassims <sableye.nico@gmail.com>
2021-02-10 15:04:07 -0800

    Update calc.ex with ability to interpret algebraic statements.

--- lib/practice/calc.ex
+++ lib/practice/calc.ex
@@ -4,21 +4,86 @@ defmodule Practice.Calc do
     num
   end
 
+  defp tag_token(token) do
+    if (token == "+" || token == "*" || token == "-" || token == "/") do
+      {:op, token}
+    else
+      {:num, parse_float(token)}
+    end
+  end
+
+  defp precedence(op) do
+    case op do
+      "+" -> 0
+      "-" -> 0
+      "*" -> 1
+      "/" -> 1
+    end
+  end
+
+  defp convertOperator(op, tokens, output, opsStack) do
+    if (Enum.count(opsStack) > 0) do
+      if (precedence(op) > precedence(hd opsStack)) do
+        convertPostfix(tokens, output, [op | opsStack])
+      else
+        convertOperator(op, tokens, [(hd opsStack) | output], tl opsStack)
+      end
+    else
+      convertPostfix(tokens, output, [op])
+    end
+  end
+
+  defp convertPostfix(tokens, output, opsStack) do
+    if (Enum.count(tokens) > 0) do
+      head = hd tokens
+      tail = tl tokens
+      case head do
+        {:num, num} -> convertPostfix(tail, [num | output], opsStack)
+        {:op, op} -> convertOperator(op, tail, output, opsStack)
+      end
+    else
+      Enum.reverse(output) ++ opsStack
+    end
+  end
+
+  defp evaluateOperator(op, stack) do
+    case op do
+      "+" -> Enum.at(stack, 1) + (hd stack)
+      "-" -> Enum.at(stack, 1) - (hd stack)
+      "*" -> Enum.at(stack, 1) * (hd stack)
+      "/" -> Enum.at(stack, 1) / (hd stack)
+    end
+  end
+
+  defp evaluatePostfix(tokens, stack) do
+    if (Enum.count(tokens) > 0) do
+      head = hd tokens
+      if (is_float(head)) do
+        evaluatePostfix((tl tokens), [head | stack])
+      else
+        evaluatePostfix((tl tokens), [ evaluateOperator(head, stack) | stack -- [(hd stack), (hd (tl stack))]])
+      end
+    else
+      hd stack
+    end
+  end
+
+  defp removeZeroes(num) do
+    if (trunc(num) == num) do
+      trunc(num)
+    else
+      num
+    end
+  end
+
   def calc(expr) do
     # This should handle +,-,*,/ with order of operations,
     # but doesn't need to handle parens.
     expr
     |> String.split(~r/\s+/)
-    |> hd
-    |> parse_float
-    |> :math.sqrt()
-
-    # Hint:
-    # expr
-    # |> split
-    # |> tag_tokens  (e.g. [+, 1] => [{:op, "+"}, {:num, 1.0}]
-    # |> convert to postfix
-    # |> reverse to prefix
-    # |> evaluate as a stack calculator using pattern matching
+    |> Enum.map(&tag_token/1)
+    |> convertPostfix([], [])
+    |> evaluatePostfix([])
+    |> removeZeroes()
   end
 end
